df3fe8ad059656e22d858e8979710ae321ed816d
Gubenkov Eugene <gubenkoved@gmail.com>
2020-07-21 22:27:57 +0400

    dcp_347_manipulate_string_to_get_min_lexicographically.py

---
+++ python/dcp_347_manipulate_string_to_get_min_lexicographically.py
@@ -0,0 +1,31 @@
+# This problem was asked by Yahoo.
+
+# You are given a string of length N and a parameter k. The string can be manipulated by taking
+# one of the first k letters and moving it to the end.
+
+# Write a program to determine the lexicographically smallest string that can be created after
+# an unlimited number of moves.
+
+# For example, suppose we are given the string daily and k = 1. The best we can create in this
+# case is ailyd.
+
+
+def f(s: str, k: int) -> str:
+    best = s
+    cur = s
+
+    while True:
+        cur = cur[k:] + cur[:k]
+
+        if cur < best:
+            best = cur
+
+        if cur == s:
+            break
+
+    return best
+
+
+print(f('daily', 1))
+print(f('bca', 2))
+print(f('dabc', 2))
